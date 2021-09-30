810479387ea1a77fb156b75079e146416b7d704d
AAM POWER DEVELOPER <AAM POWER DEVELOPER@DESKTOP-FLAPIQK>
2021-09-30 14:18:19 +0500

    Online Vegetable System

--- app/src/main/java/com/example/ovsystem/MainActivity.kt
+++ app/src/main/java/com/example/ovsystem/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.ovsystem
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bnv_Main.add(MeowBottomNavigation.Model(1, R.drawable.ic_add))
+        bnv_Main.add(MeowBottomNavigation.Model(2, R.drawable.ic_add))
+        bnv_Main.add(MeowBottomNavigation.Model(3, R.drawable.ic_add))
+        bnv_Main.add(MeowBottomNavigation.Model(4, R.drawable.ic_add))
+
+        bnv_Main.show(1, true)
+        replace(HomeFragment())
+        bnv_Main.setOnClickMenuListener {
+            when (it.id) {
+                1 -> replace(HomeFragment())
+                2 -> replace(HomeFragment())
+                3 -> replace(HomeFragment())
+                4 -> replace(HomeFragment())
+            }
+
+        }
+
+
+    }
+
+    private fun replace(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.frame, fragment).commit()
     }
 }
